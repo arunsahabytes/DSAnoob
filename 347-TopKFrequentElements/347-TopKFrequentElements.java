bf34bad9ff27ce00f5fb68777cd7e0fccd31b57e
Arun Saha <96629404+aronjublo@users.noreply.github.com>
2025-05-17 23:12:41 +0530

    File updated at 5/17/2025, 11:12:41 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,36 +1,30 @@
-// Last updated: 5/16/2025, 11:57:21 PM
-public class Solution {
-
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freqMap = new HashMap<>();
-
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+// Last updated: 5/17/2025, 11:12:41 PM
+public class Solution{
+    int[] topKFrequent(int[] nums, int k){
+    Map<Integer,Integer> freqMap = new HashMap<>();
+        for (int num : nums){
+        freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-
         List<Integer>[] buckets = new List[nums.length + 1];
 
-        for (int key : freqMap.keySet()) {
+        for (int key: freqMap.keySet()){
             int freq = freqMap.get(key);
             if (buckets[freq] == null) {
                 buckets[freq] = new ArrayList<>();
             }
             buckets[freq].add(key);
         }
+            List<Integer> resultList = new ArrayList<>();
 
-        List<Integer> resultList = new ArrayList<>();
-
-        for (int i = buckets.length - 1; i >= 0 && resultList.size() < k; i--) {
-            if (buckets[i] != null) {
-                resultList.addAll(buckets[i]);
+            for (int i = buckets.length - 1; i >= 0 && resultList.size() < k; i--) {
+                if (buckets[i] != null) {
+                    resultList.addAll(buckets[i]);
+                }
             }
+            int[] result = new int[k];
+            for (int j = 0; j < k; j++) {
+                result[j] = resultList.get(j);
+            }
+            return result;
         }
-
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = resultList.get(i);
-        }
-
-        return result;
     }
-}
