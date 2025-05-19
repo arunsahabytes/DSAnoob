926b57bd59590557ebc1670ec8f832a74b4d0202
Arun Saha <96629404+aronjublo@users.noreply.github.com>
2025-05-19 23:43:30 +0530

    File updated at 5/19/2025, 11:43:29 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,18 +1,25 @@
-// Last updated: 5/18/2025, 11:57:31 PM
+// Last updated: 5/19/2025, 11:43:29 PM
 public class Solution {
+
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
         int[] result = new int[n];
 
-        result[0] = 1;
+        prefix[0] = 1; 
         for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        suffix[n - 1] = 1; 
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         }
 
-        int R = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] = result[i] * R;
-            R = R * nums[i];
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix[i] * suffix[i];
         }
 
         return result;
