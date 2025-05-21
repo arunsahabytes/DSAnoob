f512fe42ebd8211e3d906b83618ffac611b5bc3d
Arun Saha <96629404+aronjublo@users.noreply.github.com>
2025-05-21 15:45:02 +0530

    Create file

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,30 @@
+// Last updated: 5/21/2025, 3:45:01 PM
+public class Solution {
+    public boolean isPalindrome(String s) {
+
+        if(s==null) return true;
+
+        StringBuilder sb = new StringBuilder();
+        for(char c: s.toCharArray())
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        String neword = sb.toString();
+
+        if(neword.isEmpty()) return true;
+
+        int left = 0;
+        int right = neword.length() - 1;
+
+        while(left<right){
+            if(neword.charAt(left) != neword.charAt(right)){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
